d4eb4c973ee03f14c1c43e1da3a6f971375d1b50
Marc pr <marc.rene@web.de>
2022-04-12 19:46:15 +0200

    remove unnessecary comments

--- projects/landing-page/js/app.js
+++ projects/landing-page/js/app.js
@@ -1,42 +1,8 @@
-/**
- * 
- * Manipulating the DOM exercise.
- * Exercise programmatically builds navigation,
- * scrolls to anchors from navigation,
- * and highlights section in viewport upon scrolling.
- * 
- * Dependencies: None
- * 
- * JS Version: ES2015/ES6
- * 
- * JS Standard: ESlint
- * 
-*/
 
-/**
- * Comments should be present at the beginning of each procedure and class.
- * Great to have comments before crucial code sections within the procedure.
-*/
+// Define Global Variables
 
-/**
- * Define Global Variables
- * 
-*/
 const   sections    = document.querySelectorAll("section"),
         navBar      = document.getElementById("navbar__list");
-
-/**
- * End Global Variables
- * Start Helper Functions
- * 
-*/
-
-
-/**
- * End Helper Functions
- * Begin Main Functions
- * 
-*/
 
 // build the nav
 for (let i=0; i <= sections.length-1; i++) {
@@ -75,17 +41,5 @@ navBar.addEventListener("click", (e) =>{
         }
     }
 })
-
-/**
- * End Main Functions
- * Begin Events
- * 
-*/
-
-// Build menu 
-
-// Scroll to section on link click
-
-// Set sections as active
 
 
